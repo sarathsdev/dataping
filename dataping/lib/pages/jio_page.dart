5a08f622e3bdfb4f9c46258c3ef9baf0da9e7702
sarathsdev <sarathsivan099@gmail.com>
2022-03-14 09:31:17 +0000

    __updated__

--- dataping/lib/pages/jio_page.dart
+++ dataping/lib/pages/jio_page.dart
@@ -12,8 +12,41 @@ class _SecondRouteState extends State<SecondRoute> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text("Jio"),
-      backgroundColor: const Color.fromARGB(185, 253, 49, 100),
-    ));
+          title: Text("Jio"),
+          backgroundColor: const Color.fromARGB(185, 253, 49, 100),
+        ),
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(20.0),
+          child: Table(
+            border: TableBorder.all(color: Colors.black),
+            children: [
+              TableRow(children: [
+                Text('Cell 1'),
+                Text('Cell 2'),
+                Text('Cell 3'),
+                Text('Cell 4'),
+              ]),
+              TableRow(children: [
+                Text('Cell 1'),
+                Text('Cell 2'),
+                Text('Cell 3'),
+                Text('Cell 4'),
+              ]),
+              TableRow(children: [
+                Text('Cell 1'),
+                Text('Cell 2'),
+                Text('Cell 3'),
+                Text('Cell 4'),
+              ]),
+              TableRow(children: [
+                Text('Cell 1'),
+                Text('Cell 2'),
+                Text('Cell 3'),
+                Text('Cell 4'),
+              ])
+            ],
+          ),
+        ));
   }
 }
