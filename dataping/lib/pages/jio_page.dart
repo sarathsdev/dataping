7fa5fb2fa17202dfa6a42b6702829004d8ce1f8e
sarathsdev <sarathsivan099@gmail.com>
2022-06-06 00:06:11 +0530

    updated with algo-

--- dataping/lib/pages/jio_page.dart
+++ dataping/lib/pages/jio_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'jioalgo.dart';
 
 class SecondRoute extends StatefulWidget {
   const SecondRoute({Key? key}) : super(key: key);
@@ -10,14 +11,10 @@ class SecondRoute extends StatefulWidget {
 class _SecondRouteState extends State<SecondRoute> {
   
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  TextEditingController priceEditingController = TextEditingController();
+  TextEditingController _price = TextEditingController();
   
   @override
-  void dispose() {
-  super.dispose();
-  priceEditingController.dispose();
-  
-}
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -33,11 +30,13 @@ class _SecondRouteState extends State<SecondRoute> {
         child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          SizedBox(height:30.0),
+          Center(child: Text('Enter  the Data Pack Price:'),),
           TextFormField(
-            controller: priceEditingController,
+            controller: _price,
             keyboardType: TextInputType.number,
             decoration: const InputDecoration(
-              hintText: 'Input the Data Pack Price',
+              //hintText: 'Input the Data Pack Price',
             ),
             validator: (String? value) {
               if (value == null || value.isEmpty) {
@@ -58,9 +57,7 @@ class _SecondRouteState extends State<SecondRoute> {
                 // the form is invalid.
                 if (_formKey.currentState!.validate()) {
                   // Process data.
-                  var value1 = priceEditingController.text;
-                  print('Validity:'+ value1);
-                
+                  Navigator.of(context).push(MaterialPageRoute(builder:((context) => Algo(price: _price.text))));           
                 }
               },
               child: const Text('Submit'),
@@ -73,10 +70,12 @@ class _SecondRouteState extends State<SecondRoute> {
       ),
      ),
          ),
-         
+        
        ],
      ), 
      ), 
     );
   }
 }
+
+
---
+++ dataping/lib/pages/jioalgo.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: deprecated_member_use
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class Algo extends StatefulWidget {
+  final String? price;
+  Algo({Key? key, required this.price}) : super(key: key);
+
+  @override
+  State<Algo> createState() => _AlgoState();
+}
+
+class _AlgoState extends State<Algo> {
+  String newvalue = '';
+  var pp;
+  String abc = '';
+
+  /*checkmethodvalue(var pp) {
+  if (pp > 100) {
+    print('Yes >100');
+  }
+}
+*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(185, 253, 49, 100),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            //Text('${widget.price}'),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.pink),
+              onPressed: () {
+                //pp = '${widget.price}';
+                setState(() {
+                  newvalue = ('${widget.price}');
+                  pp = int.parse(newvalue);
+                  if (pp < 100) {
+                    abc = 'a';
+                  }
+                  ;
+                  if (pp > 100 && pp < 500) {
+                    abc = 'b';
+                  }
+                  ;
+                  if (pp > 500 && pp < 1000) {
+                    abc = 'c';
+                  }
+                  ;
+                });
+
+                //print(pp.runtimeType);
+              },
+              // ignore: prefer_const_constructors
+        
+           
+              child: Text('Click here for results'),
+            ),
+
+            Center(
+                child: abc == 'a'
+                    ? new Container(
+                        child: Text('<100'),
+                      )
+                    : new Container(
+                        child: Text('.'),
+                      )),
+            Center(
+                child: abc == 'b'
+                    ? new Container(
+                        child: Text('100<500'),
+                      )
+                    : new Container(
+                        child: Text('.'),
+                      )),
+            Center(
+                child: abc == 'c'
+                    ? new Container(
+                        child: Text('500<1000'),
+                      )
+                    : new Container(
+                        child: Text('.'),
+                      )),
+
+            //checkmethodvalue(pp),
+          ],
+        ),
+
+        //child:Text('Price: ${widget.price}'),),
+      ),
+    );
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  // TODO: implement build
+  throw UnimplementedError();
+}
